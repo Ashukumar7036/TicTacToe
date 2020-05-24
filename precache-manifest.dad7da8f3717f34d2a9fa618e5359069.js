3e1e1418e5a089e1fdb4677806cfd982ab784a54
ashutoshkumar <ashukumarrock92471@gmail.com>
2020-05-24 19:35:21 +0530

    Updates

---
+++ precache-manifest.dad7da8f3717f34d2a9fa618e5359069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c657bbc76f0f80483eafd813c045e02a",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "2692bd2e447e0738f378",
+    "url": "/TicTacToe/static/css/main.238e70cb.chunk.css"
+  },
+  {
+    "revision": "a740ed8f3265a1da9ecc",
+    "url": "/TicTacToe/static/js/2.af71590a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.af71590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2692bd2e447e0738f378",
+    "url": "/TicTacToe/static/js/main.506fe05b.chunk.js"
+  },
+  {
+    "revision": "f69c0c1d047e18285808",
+    "url": "/TicTacToe/static/js/runtime-main.8e14d025.js"
+  }
+]);
